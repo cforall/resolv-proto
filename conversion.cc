e49d853e5b52d0d877ddb97e6d8af7c06639b1c6
Aaron Moss <a3moss@uwaterloo.ca>
2016-07-29 15:21:33 -0400

    Conversion graph now generates set up for BFS

--- conversion.cc
+++ conversion.cc
@@ -19,30 +19,24 @@ std::ostream& operator<< ( std::ostream& out, const ConversionGraph& g ) {
 ConversionGraph make_conversions( const SortedSet<ConcType>& types ) {
 	ConversionGraph g;
 	
-	// loop over from types
-	for ( auto from = types.begin(); from != types.end(); ++from ) {
-		// do safe conversions
-		auto to = from;
-		++to;
-		while ( to != types.end() ) {
-			Brw<ConcType> f = brw( *from );
-			Brw<ConcType> t = brw( *to );
-			g.insert( f, t, Cost::from_diff( t->id() - f->id() ) );
-			
-			++to;
-		}
-		
-		// do unsafe conversions
-		if ( from == types.begin() ) continue; // --types.begin() is invalid
-		to = from;
-		do {
-			--to;
-			
-			Brw<ConcType> f = brw( *from );
-			Brw<ConcType> t = brw( *to );
-			g.insert( f, t, Cost::from_diff( t->id() - f->id() ) );
-		} while ( to != types.begin() );
-	}
-	
+	// no conversions for empty or single-element set
+	if ( types.size() <= 1 ) return g;
+
+	auto high = types.begin();
+	auto low = high;
+	++high;
+	do {
+		Brw<ConcType> lo = brw( *low );
+		Brw<ConcType> hi = brw( *high );
+
+		// safe conversion
+		g.insert( lo, hi, Cost::from_diff( hi->id() - lo->id() ) );
+		// unsafe conversion
+		g.insert( hi, lo, Cost::from_diff( lo->id() - hi->id() ) );
+
+		low = high;
+		++high;
+	} while ( high != types.end() );
+
 	return g;
 }
